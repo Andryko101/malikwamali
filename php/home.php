e060f6293cc08e7efcefbb8f31ae1c3ea92a1387
andyryko101 <andymakari58@gmail,com>
2025-03-31 19:26:30 +0300

    Fixed merge conflicts

--- php/home.php
+++ php/home.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <?php
    include("database.php"); 
     session_start();
@@ -10,25 +8,11 @@
         exit();
     }
 ?>
->>>>>>> 811d51f (PHP files for malikwamali)
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Homepage</title>
-</head>
-<body>
-    <p onclick="redirectfun()">Welcome to mali-kwa-mali</p>    
-</body>
-<script type="text/javascript">
-    function redirectfun(){
-        window.location.href="categories.php";
-    }
-</script>
-</html>
-=======
     <title>Dedan Deals | Home</title>
     <link href="../css/homepage.css" rel="stylesheet">
 </head>
