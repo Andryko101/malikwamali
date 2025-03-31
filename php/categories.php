de39195dee7e3852fb26ebaf2b2ca2b7d1842030
andyryko101 <andymakari58@gmail,com>
2025-03-31 19:45:44 +0300

    Fixed merge conflicts

--- php/categories.php
+++ php/categories.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <?php
    include("database.php"); 
     session_start();
@@ -13,44 +11,11 @@ $categoryResult = mysqli_query($conn, $categoryQuery);
 $productQuery = "SELECT id, name, image, price, description, category FROM products ORDER BY created_at DESC LIMIT 8";
 $productResult = mysqli_query($conn, $productQuery);
 ?>
-
->>>>>>> 811d51f (PHP files for malikwamali)
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Document</title>
-    <link href="../css/home.css" rel="stylesheet">
-</head>
-<div id="logo"><img src="../images/logo.png" rel="logo"></div>
-<body style="background-image: url('../images/background.jpeg'); background-attachment: 
-fixed; background-repeat: no-repeat; background-size: cover; background-position: center;">
-    <h2>Welcome to mali-kwa-mali</h2>
-    <nav class="navbar">
-        <a href="home.php">home</a>
-        <a href="become_seller.php">Become a seller</a>
-        <a href="">about</a>
-        <a href="seller.php">My account</a>
-        <a href="login.php" style="display:inline; justify-content: right;  ">Log out</a>
-    </nav>
-    <!--Displaying products-->
-    <div class="content">
-    <div class="image" onclick="redirectfun()"><img class="img" src="../images/chair1.jpeg" alt="object"><div>Chair</div></div>
-    <div class="image"><img class="img" src="../images/chair1.jpeg" alt="object"><div>Casual Chairs</div></div>
-    <div class="image"><img class="img" src="../images/chair2.jpeg" alt="object"><div>Office Chairs</div></div>
-    <div class="image"><img class="img" src="../images/table.jpeg" alt="object"><div>Table</div></div>
-    <div class="image"><img class="img" src="../images/studyDesk.jpeg" alt="object"><div>Study desk</div></div>
-</div>
-</body>
-<script type="text/javascript">
-    function redirectfun(){
-        window.location.href="login.php";
-    }
-</script>
-</html>
-=======
     <title>Categories - Mali-Kwa-Mali</title>
     <link href="../css/categories.css" rel="stylesheet">
 </head>
@@ -116,4 +81,3 @@ fixed; background-repeat: no-repeat; background-size: cover; background-position
 </html>
 
 <?php mysqli_close($conn); ?>
->>>>>>> 811d51f (PHP files for malikwamali)
