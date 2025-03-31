cc85c033b57aeeaac3444b5845af41ec1ca2dc7c
andyryko101 <andymakari58@gmail,com>
2025-03-31 18:46:54 +0300

    Fixed merged code from git merge

--- php/signup.php
+++ php/signup.php
@@ -4,15 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
-<<<<<<< HEAD
-    <link href="../css/style.css" rel="stylesheet">
-</head>
-<body style="background-image:url('../images/background.jpeg');
-=======
     <link href="../css/signup.css" rel="stylesheet">
 </head>
 <body style="background-image:url('../images/background.jpg');
->>>>>>> 811d51f (PHP files for malikwamali)
     background-position: center;
     background-attachment: fixed;
     background-repeat: no-repeat;
@@ -20,13 +14,7 @@
     <form action="signup.php" method="post"><h2>Sign Up</h2>
     <div>Welcome to Mali kwa Mali. Create your account</div><br>
         <div>
-<<<<<<< HEAD
         <input class="input username" type="text-field" name="username" placeholder="Enter Username" required><br>
-=======
-        <input class="input username" type="text-field" name="name" placeholder="Enter full name" required><br>
-        <input class="input username" type="text-field" name="username" placeholder="Enter Username" required><br>
-        <input class="input id" type="text-field" name="id" placeholder="Enter Student ID" required><br>
->>>>>>> 811d51f (PHP files for malikwamali)
         <input class="input email" type="text-field" name="email" placeholder="Enter Email" required><br>
         <input class="input password" type="password" name="password" placeholder="Enter Password" required><br>
         <input class="input password" type="password" name="confirmpassword" placeholder="confirm Password" required><br>
@@ -34,32 +22,6 @@
         <a id="signup" href="index.php">Have an account already? Login</a>
 </div>
     </form>
-<<<<<<< HEAD
-    <?php
-    include("database.php");
-
-    
-    $username=$_POST['username'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $confirmpass=$_POST['confirmpassword'];
-
-    if(isset($_POST['submit'])){
-    if($password==$confirmpass){
-        $hash= password_hash($password, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO UserCredentials (username, email, password)
-        VALUES ('$username','$email','$hash')";
-        mysqli_query($conn, $sql);
-        echo '<script>alert("Account created successfully")</script>';
-        echo '<script type="text/javascript">
-        window.location.href = "http://localhost/mali-kwa-mali/php/home.php";
-        </script>';
-    }else{
-        echo '<script>alert("Password does not match")</script>';
-    }};
-    
-
-=======
     <footer class="footer">
         <div class="container">
             <p>&copy; 2025 Mali kwa Mali. All Rights Reserved.</p>
@@ -100,7 +62,6 @@
     }else{
         echo '<script>alert("Password does not match")</script>';
     }}}};
->>>>>>> 811d51f (PHP files for malikwamali)
     mysqli_close($conn);
 ?>
 </body>
